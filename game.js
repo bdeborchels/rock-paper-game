42f073e1df9361ffaacdc0bb4360ece4bb12f3c8
bdeborchels <b.elices.garcia@gmail.com>
2022-12-29 18:45:55 +0100

    Game function added

--- game.js
+++ game.js
@@ -27,8 +27,9 @@ function getPlayerSelection(){
 }
 
 function playRound(playerSelection, computerSelection) {
-    const playerWins = 0;
-    const computerWins = 0;
+    let winner = 0;
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerSelection();   
     console.log("Let's the round begin!");
     console.log("Computer: "+computerSelection+ "   You: "+playerSelection);
 
@@ -36,13 +37,36 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "rock" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "scissors")||
         (playerSelection === "scissors" && computerSelection === "rock")
         ){
-            computerWins++;
+            console.log("COMPUTER wins this round");
+            winner = 1;
     }
-    if((playerSelection === "rock" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "paper") ||
+    else if((playerSelection === "rock" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")){
 
-            playerWins++;
+            console.log("YOU win this round!");
+            winner = 2;
         }
+    else console.log("It's a draw!");
+
+         return winner;
     
     }
-   
+    function game(){
+        let playerWins = 0;
+        let computerWins = 0;
+        for (let i = 0; i < 5; i++) {
+            playRound(playerSelection,computerSelection);
+            if(playRound===1){
+            computerWins++;
+            }
+            if(playRound===2){
+                playerWins++;
+                }
+            }
+         if(playerWins === 3){
+            console.log("YOU WIN THIS GAME!!!");
+         }
+         if(computerWins === 3){
+            console.log("COMPUTER WINS!! Sorry :(");
+         }
+    }
